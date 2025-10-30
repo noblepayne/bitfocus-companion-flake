27609d87f9ccd044e8602c67f2333501b9871de8
Wes Payne <noblepayne@noblepayne.com>
2025-10-29 18:11:16 -0700

    feat: gate service behind option

--- module.nix
+++ module.nix
@@ -10,33 +10,32 @@
       description = "Which companion package to install.";
     };
     enable = lib.mkEnableOption "Add Bitfocus Companion to installed packages.";
+    runAsService = lib.mkEnableOption "Run Companion as a systemd service instead of just installing the package.";
     autoStart = lib.mkOption {
       type = lib.types.bool;
       default = true;
-      description = "Whether to start Bitfocus Companion automatically at boot.";
+      description = "Whether to start Bitfocus Companion automatically at boot (only used when runAsService is enabled).";
     };
     user = lib.mkOption {
       type = lib.types.str;
-      #default = "companion";
-      description = "User under which the Companion service will run.";
+      description = "User under which the Companion service will run (required when runAsService is enabled).";
     };
     group = lib.mkOption {
       type = lib.types.str;
-      #default = "users";
-      description = "Group under which the Companion service will run.";
+      description = "Group under which the Companion service will run (required when runAsService is enabled).";
     };
     openFirewall = lib.mkOption {
       type = lib.types.bool;
       default = true;
-      description = "Whether to open firewall ports for Companion.";
+      description = "Whether to open firewall ports for Companion (only used when runAsService is enabled).";
     };
   };
   cfg = config.programs.companion;
 in {
   inherit options;
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [cfg.package];
-    systemd.services.companion = {
+    environment.systemPackages = lib.mkIf (!cfg.runAsService) [cfg.package];
+    systemd.services.companion = lib.mkIf cfg.runAsService {
       description = "Bitfocus Companion";
       wantedBy = lib.mkIf cfg.autoStart ["multi-user.target"];
       after = ["network.target"];
@@ -52,7 +51,7 @@ in {
         ProtectHome = lib.mkDefault false;
       };
     };
-    networking.firewall = lib.mkIf cfg.openFirewall {
+    networking.firewall = lib.mkIf (cfg.runAsService && cfg.openFirewall) {
       allowedTCPPorts = [8000];
     };
   };
